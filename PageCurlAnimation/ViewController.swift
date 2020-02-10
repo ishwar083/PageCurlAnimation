03c00437e0b2869aa19d4faeca15cc16eaf4b40a
mac-00014 <mac-00014@example.com>
2020-02-10 16:20:32 +0530

    Initial Commit

---
+++ PageCurlAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PageCurlAnimation
+//
+//  Created by mac-00014 on 10/02/20.
+//  Copyright © 2020 Mi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
